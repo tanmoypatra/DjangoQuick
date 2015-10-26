c91c02920d587c7c2d309caadf49d0c876b95a08
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-27 00:11:41 +0530

    Add api_view function based and fixed bugs
    	modified:   snippets/views.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,35 +4,34 @@ from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-def snippet_list(request,format=None):
+
+@api_view(['GET', 'POST'])
+def snippet_list(request):
     """
-    List all snippets, or create a new snippet
-    :param request:
-    :return:
+    List all snippets, or create a new snippet.
     """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method =='POST':
-        serializer = SnippetSerializer(data = request.data)
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def snippet_detail(request, pk,format=None):
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk):
     """
     Retrieve, update or delete a snippet instance.
-    :param request:
-    :param pk:
-    :return:
     """
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
@@ -48,4 +47,3 @@ def snippet_detail(request, pk,format=None):
     elif request.method == 'DELETE':
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
