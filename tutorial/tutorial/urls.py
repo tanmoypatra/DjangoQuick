7a8b23e115623e9d298bbd073cb17736adf4e104
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-27 03:30:43 +0530

    Commit for bug fixes.
    	modified:   db.sqlite3
    	modified:   tutorial/urls.py

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -28,8 +28,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^api-auth/', include('rest_framework.urls',namespace='rest-framework')),
     url(r'^admin/', include(admin.site.urls))
 ]
