f155742dfe01b20793970db9230bb6119c7d712e
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-26 23:52:35 +0530

    Add snippet_detail to views.py
    	modified:   snippets/views.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-def snippet_list(request):
+def snippet_list(request,format=None):
     """
     List all snippets, or create a new snippet
     :param request:
@@ -20,4 +20,32 @@ def snippet_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def snippet_detail(request, pk,format=None):
+    """
+    Retrieve, update or delete a snippet instance.
+    :param request:
+    :param pk:
+    :return:
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
