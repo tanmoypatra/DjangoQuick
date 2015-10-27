a193247057810a3bc20b5924748885a3a2755237
tanmoypatra <tanmoy.patra009@gmail.com>
2015-10-27 12:22:08 +0530

    Add save function for models.py 	modified:   snippets/models.py

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='snippets')
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
 
     class Meta:
         ordering = ('created',)
+
+    def save(self,*args, **kwargs):
+        super(Snippet, self).save(*args,**kwargs)
+
